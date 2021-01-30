906df069c3709a1e6eac74bd4fd326f5d8d7e5e9
Yuliana Gunchevska <yuliana.gunchevska@gmail.com>
2021-01-30 18:34:45 +0200

    Create Kotaku's crawler

--- app/src/main/java/bg/sofia/uni/fmi/learn/ii/crawler/KotakuCrawler.java
+++ app/src/main/java/bg/sofia/uni/fmi/learn/ii/crawler/KotakuCrawler.java
@@ -1,5 +1,98 @@
 package bg.sofia.uni.fmi.learn.ii.crawler;
 
-public class KotakuCrawler {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class KotakuCrawler {
+	private static final String KOTAKU_URL = "https://kotaku.com/c/review";
+	
+	private WebDriver driver;
+	
+	public KotakuCrawler() {
+		System.setProperty("webdriver.chrome.driver", "D:\\java-projects\\InfRetr\\chromedriver.exe");
+		
+	    driver = new ChromeDriver();
+	    driver.manage().window().maximize();
+	    driver.get(KOTAKU_URL);
+	    
+	    // accept cookies
+	    driver.switchTo().frame("sp_message_iframe_388533");
+	    
+		try {
+			By cookies_accept = By.xpath("/html/body/div/div[3]/div[5]/button[2]");
+		    WebDriverWait wait = new WebDriverWait(driver, 10);
+		    wait.until(ExpectedConditions.elementToBeClickable(cookies_accept)).click();
+		    wait.until(ExpectedConditions.invisibilityOfElementLocated(cookies_accept));
+		} catch (Exception e) {
+			System.err.println("Error while accepting cookies");
+			e.printStackTrace();
+			throw e;
+		}
+		
+		driver.switchTo().defaultContent();
+	}
+	
+	// TODO add arg date - from which date to take reviews
+	// TODO should go to the next page automatically - another method maybe; stops when the date is met
+	public List<String> getLinksToLatestReviews() {	    
+	    // get list of all reviews
+	    WebElement reviewList = driver.findElement(By.xpath("/html/body/div[3]/div[5]/main/div/div[4]"));
+
+	    // Go through each review article 
+	    List<WebElement> articles = reviewList.findElements(By.tagName("article"));
+	    List<String> reviewLinks = new ArrayList<String>();
+	    for(WebElement article : articles) {
+	    	try {
+	    		WebElement heading = article.findElement(By.tagName("h2"));	
+	    	
+	    		List<WebElement> attributes = article.findElements(By.tagName("a"));
+		        WebElement headingRef = attributes.get(3);
+		        	
+		        WebElement date = article.findElement(By.tagName("time"));
+		        	
+	            System.out.println("Heading    : " + heading.getText());
+	            System.out.println("Date       : " + date.getAttribute("datetime"));
+	            System.out.println("Link       : " + headingRef.getAttribute("href"));
+	            System.out.println("------------------------------");
+	                
+	            reviewLinks.add(headingRef.getAttribute("href"));
+	    	} catch (NullPointerException e) {
+	    		System.err.println("Error while searching for reviews from Kotaku.");
+	    		e.printStackTrace();
+	    		continue;
+	    	}
+	    }
+	    
+	    return reviewLinks;
+	}
+	
+	// TODO store grame's info in DB
+	public void storeReviewsInDB() throws InterruptedException {
+		List<String> urls = getLinksToLatestReviews();
+		
+		for (String url : urls) {
+			KotakuReviewCrawler reviewCrawler = new KotakuReviewCrawler(driver, url);
+			
+			System.out.println(reviewCrawler.getReviewTitle());
+			//System.out.println(reviewCrawler.getReviewInfo());
+			System.out.println(reviewCrawler.getComments());
+		}
+	}
+	
+	public void closeDriver() {
+		driver.quit();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		KotakuCrawler crawler  = new KotakuCrawler();
+		crawler.storeReviewsInDB();	
+		crawler.closeDriver();
+	}
 }
---
+++ app/src/main/java/bg/sofia/uni/fmi/learn/ii/crawler/KotakuReviewCrawler.java
@@ -0,0 +1,142 @@
+package bg.sofia.uni.fmi.learn.ii.crawler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class KotakuReviewCrawler {
+	private WebDriver driver;
+	
+	public KotakuReviewCrawler(WebDriver driver, String reviewUrl) {
+		this.driver = driver;
+		driver.get(reviewUrl);
+	}
+
+	public String getReviewTitle() {	
+		// find tagger header 
+		WebElement header = driver.findElement(By.tagName("header"));
+		WebElement h1Header = header.findElement(By.tagName("h1"));
+		return h1Header.getText();
+	}
+	
+	public String getReviewInfo() {
+		List<WebElement> divs = driver.findElements(By.tagName("div"));
+		
+		StringBuilder review = new StringBuilder();
+		for (WebElement div : divs) {
+			try {
+			String divClass = div.getAttribute("class");
+				if (divClass.contains("js") && divClass.contains("post") && divClass.contains("content")) {
+					List<WebElement> paragraphs = div.findElements(By.tagName("p"));
+					for (WebElement paragraph : paragraphs) {
+						review.append(paragraph.getText()).append("\n");
+					}
+				}
+			} catch (NullPointerException e) {
+				System.out.println("The div in Kotaku review doesn't has class.");
+			}
+		}
+		
+		// remove the review box
+		List<WebElement> sections = driver.findElements(By.tagName("section"));
+		
+		StringBuilder reviewBox = new StringBuilder();
+		for (WebElement section : sections) {
+			try {
+			String sectionClass = section.getAttribute("class");
+				if (sectionClass.contains("reviewbox-inset")) {
+					List<WebElement> paragraphs = section.findElements(By.tagName("p"));
+					for (WebElement paragraph : paragraphs) {
+						reviewBox.append(paragraph.getText()).append("\n");
+					}
+				}
+			} catch (NullPointerException e) {
+				System.out.println("The div in Kotaku review doesn't has class.");
+			}
+		}
+		
+		int i = review.indexOf(reviewBox.toString());
+		if (i != -1) {
+			review.delete(i, i + reviewBox.length());
+		}
+		
+		// remove the releted reviews
+		List<WebElement> asides = driver.findElements(By.tagName("aside"));
+		
+		for (WebElement aside : asides) {
+			try {
+				String asideParagraph = aside.findElement(By.tagName("p")).getText();
+				
+				int j = review.indexOf(asideParagraph);
+				if (j != -1) {
+					review.delete(j, j + asideParagraph.length());
+				}
+			} catch (Exception e) {
+				System.err.println("WARN: Some asides doesnt have paragraphs");
+			}
+		}
+		
+		return review.toString();
+	}
+	
+	public List<String> getComments() throws InterruptedException {
+		// load more comments first
+		By seeAllRepliesButton = By.className("js_comments-button--show");
+	    WebDriverWait wait = new WebDriverWait(driver, 10);
+	    wait.until(ExpectedConditions.elementToBeClickable(seeAllRepliesButton)).click();
+	    wait.until(ExpectedConditions.invisibilityOfElementLocated(seeAllRepliesButton));
+		
+		System.err.println("SUCCESS: clicked");
+		
+		// change to iframe
+		List<String> comments = new ArrayList<String>();
+		List<WebElement> iframeCommentsTags = driver.findElements(By.className("js_comments-iframe"));
+		
+		for (WebElement tag : iframeCommentsTags) {
+			
+			if (tag.getTagName().equals("iframe")) {
+				
+				driver.switchTo().frame(tag);
+				System.err.println("SUCCESS: in the comments iframe");
+				
+				// load the rest of the comments
+				while (true) {
+					try {
+						By moreComments = By.className("js_button--pagination");
+						wait.until(ExpectedConditions.elementToBeClickable(moreComments)).click();
+					    wait.until(ExpectedConditions.invisibilityOfElementLocated(moreComments));
+					} catch (Exception e) {
+						//e.printStackTrace();
+						break;
+					}
+				}
+				
+				System.err.println("SUCCESS: all comments are loaded.");
+				
+				// store all comments 
+				List<WebElement> commentElements = driver.findElements(By.className("js_reply-content"));
+				for(WebElement commentElement : commentElements) {
+					List<WebElement> commentParagraphs  = commentElement.findElements(By.tagName("p"));
+					StringBuilder comment = new StringBuilder();
+					
+					for (WebElement paragraph : commentParagraphs) {
+						comment.append(paragraph.getText()).append("\n");
+					}
+					
+					comments.add(comment.toString());
+				}
+				 
+				
+				break;
+			}
+		}
+		driver.switchTo().defaultContent();
+		
+		return comments;
+	}
+}
